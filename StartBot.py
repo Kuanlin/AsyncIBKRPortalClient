1974ae6d6fa0fbeb99d6073c89fc1a6cff1f9270
Kuanlin <kuan.lyn.chen@gmail.com>
2024-03-17 16:36:16 +0800

    update

--- StartBot.py
+++ StartBot.py
@@ -156,9 +156,7 @@ class Bot(BotBase):
                     x: self.gp[x]*quota for x in self.gp.keys()
                 }
                 stock_quota = {
-                    x: {
-                        "quota": self.stkp[x]["in_group_proportion"]*grouped_quota[self.stkp[x]["group"]]
-                    } 
+                    x: self.stkp[x]["in_group_proportion"]*grouped_quota[self.stkp[x]["group"]]
                     for x in self.stkp.keys()
                 }
                 print(f"#QUOTA::{quota} => {grouped_quota} => {stock_quota}")
